e02467cc69a89f2d88761b5494fa8fc934f6aa38
elinard <elinard95@gmail.com>
2025-08-11 10:03:59 +0500

    +2 ex

--- tests/test_main.py
+++ tests/test_main.py
@@ -82,11 +82,6 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
-# def print_func_name(func, *args):
-#     func_name = func.__name__.replace('_', ' ').title()
-#     arg_name = ", ".join([*args])
-#     print (f'{func_name} [{arg_name}]')
-
 def print_func_name_args(func, *args):
     func_name = func.__name__.replace('_', ' ').title()
     args_str = ", ".join(map(str, args))
@@ -99,18 +94,14 @@ def test_readable_function():
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
-
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_func_name_args(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
-
-
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_func_name_args(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
-
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_func_name_args(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
